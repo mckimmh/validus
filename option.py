024f6ee3f7420b2fcb118ae40cbc899c8e52cf34
mckimmh <mckimmh@gmail.com>
2024-03-20 09:25:55 +0000

    compute value

--- option.py
+++ option.py
@@ -2,11 +2,12 @@
 Validus Case Study
 March 2024
 """
+import numpy as np
 
 class Put:
     """
     European put option.
-    Assumes u=1+nu, d=1-nu; P(u)=0.5, P(d)=0.5.
+    Assumes u=1+nu, d=1-nu; P(u)=0.5, P(d)=0.5; r=0.
     """
     
     def __init__(self, S0, K, N):
@@ -38,5 +39,27 @@ class Put:
         """
         nu : asset price increase/decrease proportion
         """
-        
 
+        # Asset prices at maturity:
+        d_vec = (1-nu)**(np.arange(self.N, -1, -1))
+        u_vec = (1+nu)**(np.arange(0, self.N+1))
+        S = self.S0 * d_vec * u_vec
+
+        # Option values at maturity
+        V = np.maximum(self.K - S, np.zeros(self.N+1))
+
+        # Step backwards through the tree
+        for i in range(self.N, 0, -1):
+            V = 0.5 * V[1:(i+1)] + 0.5 * V[0:i]
+
+        return V[0]
+    
+S0 = 1.28065
+K  = 1.28065
+N = 10
+my_option = Put(S0, K, N)
+nu = 0.05
+
+V = my_option.value(nu)
+
+print(V)
